2578e4760419fb77f8146b9c0a50278d2ef76629
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-11-12 21:45:46 -0800

    Final submission.

--- test_query.sql
+++ test_query.sql
@@ -1,21 +1,20 @@
 -- Query to check successful load
-SELECT * FROM premise;
-
-SELECT * FROM county;
-
 SELECT
-    species_tracking.species_common_name AS Species,
+	city.project_id AS Project,
+	city.approval_valuation AS Job_Cost,
+	development.job_bc_code AS Building_Code,
+	development.job_bc_code_description AS Code_Description,
+	species_tracking.species_common_name AS Species,
     species_tracking.total_sightings AS Sightings,
     species_tracking.lat AS Lat,
 	species_tracking.long AS Long,
-	city.project_id AS PROJECT,
-	city.approval_valuation AS Job_Cost
-	geographic.zip AS Zip Code,
+	geographic.zip AS Zip_Code,
 	geographic.council_district AS Council_District
-	
 FROM
-    species_tracking
-INNER JOIN species_tracking ON 
+	species_tracking
+INNER JOIN species_tracking ON
 ROUND(species_tracking.lat , 2 ) AND ROUND(geographic.lat_job , 2 ) AND
-ROUND(species_tracking.long , 2 ) AND ROUND((geographic.long_job , 2) 
+ROUND(species_tracking.long , 2 ) AND ROUND((geographic.long_job , 2)
+INNER JOIN geographic ON geographic.project_id = city.project_id 
+INNER JOIN development on development.project_id = city.project_id
 ;
