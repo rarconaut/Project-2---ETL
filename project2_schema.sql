03d80b75abf6de092185933be2639586f1b992e6
geneberry3 <69098033+geneberry3@users.noreply.github.com>
2020-11-12 20:03:46 -0800

    Add files via upload

--- project2_schema.sql
+++ project2_schema.sql
@@ -1,16 +1,55 @@
 create table species_tracking (
 	species_scientific_name	VARCHAR(255),
 	species_common_name	VARCHAR(255),
-	observation_date date,
+	observation_date DATE,
 	track_type VARCHAR(255),
 	total_sightings	INT,
 	vegetation_category	VARCHAR(255),
 	location_name VARCHAR(255),
 	reserve_name VARCHAR(255),
-	lat	VARCHAR,
-	long VARCHAR
+	lat	REAL,
+	long REAL
+);
+create table city (
+	project_id INT,
+	date_project_create DATE,
+	date_project_complete DATE,
+	project_title VARCHAR,
+	project_scope VARCHAR,
+	address_job VARCHAR,
+	job_bc_code INT,
+	lat_job REAL,
+	lng_job REAL,
+	approval_type VARCHAR,
+	approval_valuation MONEY,
+);
+create table geographic(
+	lat_job REAL,
+	lng_job REAL,
+	council_district INT,
+	zip INT,
 );
 select * from species_tracking
 ;
-drop table species_tracking
+select * from city
+;
+select * from geographic
+;
+drop table **DEFINE**
+;
+SELECT
+    species_tracking.species_common_name AS Species,
+    species_tracking.total_sightings AS Sightings,
+    species_tracking.lat AS Lat,
+	species_tracking.long AS Long,
+	city.project_id AS PROJECT,
+	city.approval_valuation AS Job_Cost
+	geographic.zip AS Zip Code,
+	geographic.council_district AS Council_District
+	
+FROM
+    species_tracking
+INNER JOIN species_tracking ON 
+ROUND(species_tracking.lat , 2 ) AND ROUND(geographic.lat_job , 2 ) AND
+ROUND(species_tracking.long , 2 ) AND ROUND((geographic.long_job , 2) 
 ;
